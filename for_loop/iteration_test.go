3810963525239ad2729d469acd286cd72a91cd35
3dw1nM0535 <workockmoses@gmail.com>
2022-04-01 00:02:12 +0300

    for_loop

---
+++ for_loop/iteration_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	got := Repeat("w", 190)
+	want := "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("w", 500)
+	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("a", 10)
+	fmt.Println(repeat)
+	// Output: aaaaaaaaaa
+}
