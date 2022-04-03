6a2600f77450ba3a65773fb8a13424c7e940cb46
3dw1nM0535 <workockmoses@gmail.com>
2022-04-03 21:18:19 +0300

    Refactor

--- slices/sum_test.go
+++ slices/sum_test.go
@@ -19,11 +19,32 @@ func TestSum(t *testing.T) {
 	})
 }
 
-func TestAll(t *testing.T) {
+func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2, 3}, []int{4})
 	want := []int{6, 4}
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
+}
+
+func TestAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("make the sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{2, 3, 4}, []int{4})
+		want := []int{7, 0}
+
+		checkSums(t, got, want)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{4})
+		want := []int{0, 0}
+
+		checkSums(t, got, want)
+	})
 }
